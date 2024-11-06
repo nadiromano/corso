33488ac1ff13fbc44f6900e81a5a6c3ec1ad96de
giuseppe romano <giuseppe.romano@euris.it>
2024-11-06 10:20:07 +0100

    add files

--- base.js
+++ base.js
@@ -1,1 +1,50 @@
-console.log('test');
+//VALORI E VARIABILI DATA TYPES LET CONST BASIC OPERATOR TEMPLATE LITERAL
+
+// Dichiara le variabili chiamate paese, continente e popolazione e assegna i loro valori in base al tuo paese (popolazione in milioni).
+
+//Dichiara una variabile chiamata isIsland e imposta il suo valore in base al tuo paese. La variabile dovrebbe contenere un valore booleano. Dichiara anche una variabile lingua, ma non assegnarle ancora alcun valore. Registra i tipi di isola, popolazione, paese e lingua sulla console.
+
+//Imposta il valore della lingua sulla lingua parlata nel luogo in cui vivi (alcuni paesi hanno più lingue, ma scegline solo una).Pensa a quali variabili dovrebbero essere variabili const (quali valori non cambieranno mai e quali potrebbero cambiare?). Quindi, modifica queste variabili in const.Prova a cambiare ora una delle variabili modificate e osserva cosa succede.
+
+//Se il tuo paese si dividesse a metà e ciascuna metà contenesse metà della popolazione, quante persone vivrebbero in ciascuna metà? Aumenta la popolazione del tuo paese di 1 e registra il risultato sulla console.La Finlandia ha una popolazione di 6 milioni di abitanti. Il tuo paese ha più abitanti della Finlandia? La popolazione media di un paese è di 33 milioni di persone. Il tuo paese ha meno abitanti della media del paese?In base alle variabili che hai creato, crea una nuova descrizione della variabile che contenga una stringa con questo formato: "Il Portogallo è in Europa e i suoi 11 milioni di persone parlano portoghese".
+
+//Ricrea la variabile descrizione dall'ultimo compito, questa volta utilizzando i template literal
+
+//PRENDI DECISIONI
+
+//Se la popolazione del tuo paese è superiore a 33 milioni, registra una stringa come questa sulla console: "La popolazione del Portogallo è 22 milioni sotto la media" (22 è la media di 33 meno la popolazione del paese).Dopo aver controllato il risultato, modifica temporaneamente la popolazione a 13 e poi a 130. Osserva i diversi risultati e ripristina la popolazione all'originale.
+
+//TYPE COERCISION
+
+// Prevedere il risultato di queste 5 operazioni senza eseguirle:
+
+// console.log('9' - '5'); // -> ?
+// console.log('19' - '13' + '17'); // -> ?
+// console.log('19' - '13' + 17); // -> ?
+// console.log('123' < 57); // -> ?
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ?
+// Esegui le operazioni per verificare se avevi ragione.
+
+// LOGICAL OPERATOR
+
+// Diciamo che Sarah sta cercando un nuovo paese in cui vivere. Vuole vivere in un paese che parli inglese, abbia meno di 50 milioni di abitanti e non sia un'isola. Scrivi un'istruzione if per aiutare Sarah a capire se il tuo paese è adatto a lei. Dovrai scrivere una condizione che tenga conto di tutti i criteri di Sarah. Se il tuo è il paese giusto, registra una stringa come questa "Dovresti vivere in Portogallo :)". In caso contrario, log 'Il Portogallo non soddisfa i tuoi criteri :('. Probabilmente il tuo Paese non soddisfa tutti i criteri. Quindi torna indietro e modifica temporaneamente alcune variabili per rendere vera la condizione
+
+// SWITCH STATEMENT
+
+// Utilizzare un'istruzione switch per registrare la seguente stringa per la lingua specificata:
+
+// cinese o mandarino: 'MAGGIOR numero di madrelingua!';
+
+// spagnolo: '2° posto per numero di madrelingua';
+
+// inglese: '3° posto';
+
+// hindi: 'Numero 4';
+
+// arabo: 'quinta lingua più parlata';
+
+// per tutti gli altri inserisci semplicemente "poco utilizzata ma fantastica".
+
+//TERNARY
+
+// Se la popolazione del tuo Paese è superiore a 33 milioni, utilizza l'operatore ternario per registrare una stringa come questa sulla console: "La popolazione del Portogallo è superiore alla media". Altrimenti, registra semplicemente "La popolazione del Portogallo è inferiore alla media". Nota come cambia solo una parola tra queste due frasi!Dopo aver controllato il risultato, modifica temporaneamente la popolazione a 13 e poi a 130. Osserva i diversi risultati e ripristina la popolazione all'originale.
